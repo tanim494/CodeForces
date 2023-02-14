2f9306611cc6d80f8a4cdc59d3a6ce929bf7e4b4
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-02-14 22:33:09 +0600

    Create (S-1)I.  Welcome for you with Conditions.java

---
+++ (S-1)I.  Welcome for you with Conditions.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a >= b){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
