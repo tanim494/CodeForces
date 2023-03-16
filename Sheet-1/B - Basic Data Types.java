4ca5e24add3b6054e7ae9c52e8d0b2e21685bd3b
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-03-16 23:56:35 +0600

    Create B - Basic Data Types.java

---
+++ Sheet-1/B - Basic Data Types.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CodeForces {
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+long b = sc.nextLong();
+char c = sc.next().charAt(0);
+float d = sc.nextFloat();
+double e = sc.nextDouble();
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}
+} 
