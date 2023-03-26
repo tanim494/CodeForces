59599397fbbddef4935643be5a3f4d61c1490696
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-03-26 13:27:54 +0600

    Update H - One Prime.java

--- Sheet-2/H - One Prime.java
+++ Sheet-2/H - One Prime.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        int n = sc.nextInt();
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                flag++;
+                break;
+            }
+        }
+        if (n == 0 || n == 1 || flag == 1) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+   }
+}
+
+/*
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if (n == 2) {
             System.out.println("YES");
@@ -20,3 +41,4 @@ public class Main {
         System.out.println("YES");
    }
 }
+*/
