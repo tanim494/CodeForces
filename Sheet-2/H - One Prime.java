78ddaad19263b46550c3a00a3a320d604e9ea0bd
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-03-26 13:22:10 +0600

    Create H - One Prime.java

---
+++ Sheet-2/H - One Prime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 2) {
+            System.out.println("YES");
+            return;
+        }
+        if (n == 0 || n == 1 || n % 2 == 0) {
+            System.out.println("NO");
+            return;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+   }
+}
