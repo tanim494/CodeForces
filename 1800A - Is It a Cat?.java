c476d45be83c0f2beb907cd749627bf56ad24ed4
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-03-29 13:03:24 +0600

    Update 1800A - Is It a Cat?.java

--- 1800A - Is It a Cat?.java
+++ 1800A - Is It a Cat?.java
@@ -6,6 +6,35 @@ public class Main {
         int test = sc.nextInt();
         for (int i = 0; i < test; i++) {
             int wLen = sc.nextInt();
+            sc.nextLine(); 
+            String word = sc.nextLine().toLowerCase();
+            char[] temp = new char[wLen];
+            temp[0] = word.charAt(0);
+            int j = 1;
+            for (int k = 1; k < wLen; k++) {
+                if (word.charAt(k) != word.charAt(k - 1)) {
+                    temp[j] = word.charAt(k);
+                    j++;
+                }
+            }
+            String tempStr = new String(temp, 0, j);
+            boolean working = tempStr.equals("meow");
+          
+            System.out.println(working ? "YES" : "NO");
+        }
+    }
+}
+
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (int i = 0; i < test; i++) {
+            int wLen = sc.nextInt();
             sc.nextLine(); // read the newline character
             String word = sc.nextLine().toLowerCase();
             StringBuilder temp = new StringBuilder();
