5fe3b75d81902ed8fc3c30e2070523368a80ff1b
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-04-05 21:58:24 +0600

    Update 1811A - Insert Digit.java

--- 1811A - Insert Digit.java
+++ 1811A - Insert Digit.java
@@ -1,6 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (int i = 0; i < test; i++) {
+            int n = sc.nextInt();
+            int d = sc.nextInt();
+            String num = sc.next();
+            StringBuilder ans = new StringBuilder();
+            boolean inserted = false;
+            for (int j = 0; j < n; j++) {
+                int digit = num.charAt(j) - '0';
+                if (!inserted && digit < d) {
+                    ans.append(d);
+                    inserted = true;
+                }
+                ans.append(digit);
+            }
+            if (!inserted) {
+                ans.append(d);
+            }
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+
+/*
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
